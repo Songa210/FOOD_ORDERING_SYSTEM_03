aa34cbac6899127e8dcf00f0b8e25dc835704770
Elyse NIYOMWUNGERE <elysesonga1997@gmail.com>
2024-02-06 15:18:28 +0200

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAyT7bIn_cacv5n4pXGnUWMDYrmo2UulvE",
+  authDomain: "fir-foodi-client-b04bb.firebaseapp.com",
+  projectId: "fir-foodi-client-b04bb",
+  storageBucket: "fir-foodi-client-b04bb.appspot.com",
+  messagingSenderId: "671308570325",
+  appId: "1:671308570325:web:6019d769d1aa34ca7ccc5c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/router/Router.jsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+import Home from "../pages/home/Home";
+import Menu from "../pages/shop/Menu";
+import Signup from "../components/Signup";
+import PrivateRouter from "../PrivateRouter/PrivateRouter";
+import updateProfile from "../pages/dashboard/UpdateProfile";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/update-profile",
+        element: <updateProfile />,
+      },
+    ],
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+]);
+
+export default router;
